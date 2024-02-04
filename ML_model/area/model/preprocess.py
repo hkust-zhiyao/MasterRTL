6f03e374baf6773f84652f6df84008ff239e7cde
fangwenji <wfang838@connect.hkust-gz.edu.cn>
2024-02-04 21:11:08 +0800

    fix bugs in ML model

--- ML_model/area/model/preprocess.py
+++ ML_model/area/model/preprocess.py
@@ -13,13 +13,7 @@ import sys
 from pathlib import Path
 folder = Path(__file__).parent.parent.parent.parent
 sys.path.append(str(folder))
-from utils.statistics import *
 
-import sys
-sys.path.append('/data/user/masterRTL/')
-sys.path.append('/data/user/masterRTL/utils')
-sys.path.append('/data/user/masterRTL/utils/metrics.py')
-from utils.metrics import get_metrics
 
 bench_type = "area_pwr"
 feat_type = "feat_area"
@@ -63,55 +57,4 @@ def load_data_sep():
 
 
 def draw_fig_kf(title, y_pred, y_test, method, train_test):
-    print(title)
-    r = get_metrics(y_pred, y_test)
-    if title != 'Total Area':
-        return
-    if r > 0.97:
-        pass
-    #     with open ('../data/pred.pkl', 'wb') as f:
-    #         pickle.dump(y_pred, f)
-    #     with open ('../data/real.pkl', 'wb') as f:
-    #         pickle.dump(y_test, f)
-    #     exit()
-    # else:
-    #     return
-    mse = metrics.mean_squared_error(normalization(y_test), normalization(y_pred), squared=True)
-    mse = round(mse, 3)
-    mspe = mspe_cal(y_test, y_pred)
-    mspe = round(mspe, 3)
-    rmse = metrics.mean_squared_error(normalization(y_test), normalization(y_pred), squared=False)
-    rmse = round(rmse, 3)
-    rmspe = rmspe_cal(y_test, y_pred)
-    rmspe = round(rmspe, 3)
-    mape_val = mape(y_pred, y_test)
-    mape_val = round(mape_val)
-    print("MAPE:", mape_val)
-    print("RMSE:", rmse)
-    r, p = stats.pearsonr(y_test, y_pred)
-    r = round(r, 3)
-    print("R:", r)
-    r2 = metrics.r2_score(y_test, y_pred)
-    r2 = round(r2, 3)
-    print("R2:", r2)
-    fig, ax = plt.subplots(tight_layout = True)
-    if train_test == 'Train':
-        ax.scatter(y_test, y_pred, c="b", alpha= 0.6)
-    elif train_test == 'Test':
-        ax.scatter(y_test, y_pred, c="orange", alpha= 0.6)
-    else:
-        assert False
-    ax.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], ls="--", c='r', alpha = 0.2)
-    ax.set_xlabel('Measured', fontproperties='Times New Roman', size=14)
-    ax.set_ylabel('Predicted', fontproperties='Times New Roman', size=14)
-    bbox = dict(boxstyle="round", fc='1', alpha=0.5)
-    plt.text( 0.05, 0.75,  fontdict={'family':'Times New Roman'}, s=f'R = {r}\nMAPE = {mape_val}%\nRMSE = {rmse}', 
-                    transform=ax.transAxes, size=14, bbox=bbox)
-    if title != 'WNS':
-        plt.xscale('log')
-        plt.yscale('log')
-    plt.title(f'{title} {train_test}', fontdict={'family':'Times New Roman', 'size':24})
-    plt.xticks(fontproperties='Times New Roman', size=14)
-    plt.yticks(fontproperties='Times New Roman', size=14)
-    plt.show()
-    plt.savefig(f"/data/user/masterRTL/figures/{method}_{title}.png", dpi=300)
+    pass
